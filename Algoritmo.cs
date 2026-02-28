dfa845e72ba48d58bc3bfc68608ddbc24f42bf06
arnie6403 <arnie6463@gmail.com>
2026-02-28 08:55:30 -0600

    Refactor Bubble Sort algorithm
    Refactor Bubble Sort implementation and remove null check.

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -25,21 +25,17 @@ public class Algoritmo
 
     public void BubbleSort(int[] arr)
     {
-        if (arr == null || arr.Length <= 1)
-            return;
-
+        // TODO: Implementar el algoritmo de Bubble Sort tradicional
         int n = arr.Length;
-
         for (int i = 0; i < n - 1; i++)
         {
-            for (int j = 0; j < n - i - 1; j++)
+            for (int j = 0; j < n-i-1; j++)
             {
                 if (arr[j] > arr[j + 1])
                 {
-                    // Intercambio (Swap)
-                    int temporal = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temporal;
+                    arr[j + 1] = temp;
                 }
             }
         }
